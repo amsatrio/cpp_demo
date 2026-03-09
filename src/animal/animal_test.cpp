47bdfa5cf6ddabd95ba0c29f22ff816a435cca6e
amsatrio <79563756+amsatrio@users.noreply.github.com>
2026-03-10 04:51:54 +0700

    update animal test case

--- src/animal/animal_test.cpp
+++ src/animal/animal_test.cpp
@@ -1,19 +1,27 @@
 #include "animal.h"
+#include <cassert>
 #include <iostream>
 #include <memory>
-#include <cassert>
 
 using namespace std;
 
 int main() {
-    unique_ptr<Animal> dog = make_unique<Dog>();
-    unique_ptr<Animal> cat = make_unique<Cat>();
-    unique_ptr<Animal> mouse = make_unique<Mouse>();
+  unique_ptr<Animal> dog = make_unique<Dog>();
+  unique_ptr<Animal> cat = make_unique<Cat>();
+  unique_ptr<Animal> mouse = make_unique<Mouse>();
 
-    assert(dog->make_sound() == "Woof!");
-    assert(cat->make_sound() == "Meow!");
-    assert(mouse->make_sound() == "Squeak!");
+  assert(dog->make_sound() == "Woof!");
+  assert(cat->make_sound() == "Meow!");
+  assert(mouse->make_sound() == "Squeak!");
 
-    cout << "All Animal tests passed (C++23)!" << endl;
-    return 0;
+  dog->set_name("d");
+  cat->set_name("c");
+  mouse->set_name("m");
+
+  assert(dog->get_name() == "d");
+  assert(cat->get_name() == "c");
+  assert(mouse->get_name() == "m");
+
+  cout << "All Animal tests passed (C++23)!" << endl;
+  return 0;
 }
